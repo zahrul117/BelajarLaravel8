c9d9488f1e4a4b8ec4b7927043fffe8afd420830
zahrul117 <zahrul0009@gmail.com>
2025-04-29 17:30:31 +0700

    Belajar strukturFolder,RoutesDanView

---
+++ public/js/script.js
@@ -0,0 +1,6 @@
+const p = document.getElementById("judul");
+
+p.addEventListener("click", function () {
+    p.textContent = "One Piece";
+    p.style.color = "red";
+});
---
+++ resources/views/about.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Halaman About</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <h1 id="judul">Ini adalah halaman About</h1>
+    <p>Luffy</p>
+    <p>Luffy@gmail.com</p>
+    <img src="img/1.jpeg" alt="">
+    <script src="js/script.js"></script>
+</body>
+</html>
